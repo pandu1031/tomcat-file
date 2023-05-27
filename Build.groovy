d0b30689d740d580f1b8cef89aa72f07ded08939
pandu1031 <chintupandu1031@gmail.com>
2023-05-28 00:55:22 +0530

    added shell commands in deploy stage

--- Build.groovy
+++ Build.groovy
@@ -4,6 +4,8 @@ pipeline{
     parameters {
         string(name: 'BRANCH_SOURCE', defaultValue: 'source', description: 'Enter source code branch ')
         string(name: 'BRANCH_PIPE', defaultValue: 'pipeline', description: 'Enter pipeline code branch')
+        string(name: 'SERVER_IP', defaultValue: '', description: 'Enter server ip')
+        
     }
     stages{
         stage("clone code"){
@@ -25,6 +27,15 @@ pipeline{
             steps{
                 println " here im uploading artifacts to s3 bucket"
                 sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://mamuu/pandu/${BRANCH_SOURCE}/${BUILD_NUMBER}/"
+            }
+        }
+        stage("Deploy"){
+            steps{
+                println "here im downloading artifacts from jenkins server to tomcat"
+                sh """
+                scp -i /tmp/mamu1031.pem /tmp/tomcatinstall.sh ec2-user@${SERVER_IP}:/tmp/
+                ssh -i /tmp/mamu1031.pem ec2-user@${SERVER_IP} "bash /tmp/tomcatinstall.sh && systemctl status tomcat"
+                """
             }
         }
     }
