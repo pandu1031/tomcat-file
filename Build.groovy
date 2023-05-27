1962f0572ffec5e76b422fc406d7f8d95c84d9a7
pandu1031 <chintupandu1031@gmail.com>
2023-05-27 23:50:46 +0530

    added the build code

---
+++ Build.groovy
@@ -0,0 +1,31 @@
+//Declarative pipeline
+pipeline{
+    agent any
+    parameters {
+        string(name: 'BRANCH_SOURCE', defaultValue: 'source', description: 'Enter source code branch ')
+        string(name: 'BRANCH_PIPE', defaultValue: 'pipeline', description: 'Enter pipeline code branch')
+    }
+    stages{
+        stage("clone code"){
+            steps{
+                println "here im cloning the code from github"
+                    git branch: '$BRANCH_SOURCE', 
+                     url: "https://github.com/pandu1031/boxfuse-sample-java-war-hello.git"
+
+            }
+        }
+        stage("Build"){
+            steps{
+                println " here im building the code"
+                sh "mvn clean package"
+                sh " ls -l /target"
+            }
+        }
+        stage("Upload artifacts"){
+            steps{
+                println " here im uploading artifacts to s3 bucket"
+                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://mamuu/pandu/${BRANCH_NAME}/${BUILD_NUMBER}/"
+            }
+        }
+    }
+}
